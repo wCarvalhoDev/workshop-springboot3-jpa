c1e2582aac1bce1955056ec6d1486e6266abd743
uevertinho <devvertinho@gmail.com>
2025-11-09 18:30:42 -0300

    User insert

--- src/main/java/com/wcarvalho/course/resources/UserResource.java
+++ src/main/java/com/wcarvalho/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.wcarvalho.course.entities.User;
 import com.wcarvalho.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 //Controller → recebe a requisição HTTP
@@ -24,14 +23,26 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    // @PostMapping → indica que o metodo responde a requisições HTTP POST.
+    // @RequestBody → diz que o User vem no corpo da requisição.
+    //  ervice.insert(obj) → salva o usuário no banco.
+    //  ServletUriComponentsBuilder... → gera a URI do novo usuário criado.
+    //  ResponseEntity.created(uri).body(obj) → retorna resposta HTTP 201 com o objeto e a URI.
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
