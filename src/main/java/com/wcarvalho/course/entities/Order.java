10010d611ca7634b5d420f1b08e2818bc1c1480a
Weverton Carvalho <wcarvalhomarcelino@gmail.com>
2025-11-09 20:14:02 -0300

    Exception handling - findById

--- src/main/java/com/wcarvalho/course/entities/Order.java
+++ src/main/java/com/wcarvalho/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.wcarvalho.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wcarvalho.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
@@ -16,6 +17,8 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     private Integer orderStatus;
--- src/main/java/com/wcarvalho/course/services/UserService.java
+++ src/main/java/com/wcarvalho/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.wcarvalho.course.services;
 
 import com.wcarvalho.course.entities.User;
 import com.wcarvalho.course.respositories.UserRepository;
+import com.wcarvalho.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,7 +27,7 @@ public class UserService {
 
     public User findById(Long id){
        Optional<User> obj = repository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
