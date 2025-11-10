a95313732e4cf7bb43a786125e29184958f9d614
Weverton Carvalho <wcarvalhomarcelino@gmail.com>
2025-11-10 08:13:10 -0300

    Exception handling - update

--- src/main/java/com/wcarvalho/course/services/UserService.java
+++ src/main/java/com/wcarvalho/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.wcarvalho.course.entities.User;
 import com.wcarvalho.course.respositories.UserRepository;
 import com.wcarvalho.course.services.exceptions.DatabaseException;
 import com.wcarvalho.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -52,9 +53,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
